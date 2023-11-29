b24ea59784224c906031f6f8dad78f5748a002be
jin <jinzhaocnfr@gmail.com>
2023-11-29 10:02:18 +0100

    footer lien <a> envoie sur page accueil avec home-url

--- footer.php
+++ footer.php
@@ -2,7 +2,10 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div class="mention">Mentions légales</div>
+<div class="mention">
+<a href="<?php echo esc_url(home_url('/')); ?>">Mentions légales
+</a>
+</div>
 </footer>
 </div>
 <?php wp_footer(); ?>
